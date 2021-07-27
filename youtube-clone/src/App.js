615da04c1118bf9dcb4550d60e7121d90167764d
Sarthak Patipati <pusarthak@gmail.com>
2021-07-26 19:16:52 -0500

    finished basic icon structure

---
+++ youtube-clone/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'; 
+import Header from './Header';
+import './App.css'; 
+
+function App() {
+  return (
+    //BEM class naming convention
+    <div className="App">
+      {/*Header*/}
+      <Header>
+
+      </Header>
+      {/*Sidebar*/}
+      {/*Recommended Videos*/}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ youtube-clone/src/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+import "./Header.css"; 
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+//import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Avatar from '@material-ui/core/Avatar';
+
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header_left">
+            <MenuIcon />
+            <img
+                className="header_logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
+                alt="Youtube logo"
+                />
+            </div>
+        
+            <div className="header_input">
+                <input type="text" />
+                <SearchIcon className="header_inputButton"/>
+            </div>
+            
+            <div className="header_icons">
+                <VideoCallIcon />
+                <AppsIcon />
+                <NotificationsIcon />
+                 <Avatar
+                    alt="Profile picture"
+                    src="https://www.facebook.com/photo/?fbid=3091555994244291&set=a.104017852998135"
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Header; 
