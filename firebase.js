f951a271eace61c700ae6f290d6ba28c15bee0db
Sarthak Patipati <pusarthak@gmail.com>
2021-07-28 17:55:48 -0500

    deployed on firebase

---
+++ firebase.js
@@ -0,0 +1,9 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD2wL6Gt13q09s_UOiCN9Tlo2kLBaJPGKo",
+//   authDomain: "clone-a0c2d.firebaseapp.com",
+//   projectId: "clone-a0c2d",
+//   storageBucket: "clone-a0c2d.appspot.com",
+//   messagingSenderId: "323439783900",
+//   appId: "1:323439783900:web:a2c8d7b6c06903c608a919",
+//   measurementId: "G-1Q182LE3MN"
+// };
